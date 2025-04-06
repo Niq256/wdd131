122fe0a84c0189c26963010d72f215d6e165c04c
Niq256 <twesigyeniclous@gmail.com>
2025-04-07 00:58:55 +0300

    Product Review Form Upload

---
+++ scripts/form-script.js
@@ -0,0 +1,24 @@
+const products = [
+    { id: "fc-1888", name: "Flux Capacitor", averagerating: 4.5 },
+    { id: "fc-2050", name: "Power Laces", averagerating: 4.7 },
+    { id: "fs-1987", name: "Time Circuits", averagerating: 3.5 },
+    { id: "ac-2000", name: "Low Voltage Reactor", averagerating: 3.9 },
+    { id: "jj-1969", name: "Warp Equalizer", averagerating: 5.0 },
+];
+
+// Populate the product options
+const productSelect = document.getElementById("product");
+products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    productSelect.appendChild(option);
+});
+
+// current year
+const yearSpan = document.getElementById('currentyear');
+yearSpan.textContent = new Date().getFullYear();
+
+// last modified date
+const lastModifiedPara = document.getElementById('lastModified');
+lastModifiedPara.textContent = `Last updated: ${document.lastModified}`;
